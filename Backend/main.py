07eb87139371a8e737b613e7fca2e75075486a32
Brighty397 <brightyjobin976@gmail.com>
2025-03-10 09:57:10 +0530

    changes to main

--- Backend/main.py
+++ Backend/main.py
@@ -27,6 +27,15 @@ class Recipe(BaseModel):
     TotalTime:str
     RecipeIngredientParts:list[str]
     Calories:float
+    FatContent:float
+    SaturatedFatContent:float
+    CholesterolContent:float
+    SodiumContent:float
+    CarbohydrateContent:float
+    FiberContent:float
+    SugarContent:float
+    ProteinContent:float
+    RecipeInstructions:list[str]
  
 
 class PredictionOut(BaseModel):
@@ -36,6 +45,15 @@ class PredictionOut(BaseModel):
 @app.get("/")
 def home():
     return {"health_check": "OK"}
+
+@app.post("/predict/",response_model=PredictionOut)
+def update_item(prediction_input:PredictionIn):
+    recommendation_dataframe=recommend(dataset,prediction_input.nutrition_input,prediction_input.ingredients,prediction_input.params.dict())
+    output=output_recommended_recipes(recommendation_dataframe)
+    if output is None:
+        return {"output":None}
+    else:
+        return {"output":output}
 
 
 
