d93fc64e9c883775c0f8f746249d7835c0498577
Rajeev-08 <rajivrk08@gmail.com>
2025-03-17 05:32:05 +0530

    initial_test

---
+++ Frontend/NUTRI_CRAFT.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="NUTRI CRAFT",
+)
+
+st.write("# objective")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    The objective of the NutriCraft project is to develop a user-friendly Food and diet recommendation system 
+    that utilizes machine learning models  to generate personalized meal plans, With a focus on scalability and 
+    adaptability NutriCraft can support diverse  requirements and larger datasets making it a valuable tool for 
+    promoting better nutrition and overall well-being
+    """
+)
