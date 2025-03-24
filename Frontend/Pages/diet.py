a7940474a668d032ae94a0685237af91c352a222
Nidha-Shameer-TS <nidhu.shameer@gmail.com>
2025-03-24 07:23:32 +0300

    diet updated

--- Frontend/Pages/diet.py
+++ Frontend/Pages/diet.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-from Generate_Recommendations import Generator
+from Generate_R import Generator
 from random import uniform as rnd
-from ImageFinder.ImageFinder import get_images_links as find_image
+from Find_img.img import get_images_links as find_image
 from streamlit_echarts import st_echarts
 
 st.set_page_config(page_title="Automatic Diet Recommendation", page_icon="💪",layout="wide")
